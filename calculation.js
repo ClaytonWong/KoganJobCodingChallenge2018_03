8b4e15f7878c328de6af73de11c84ab9e5e53091
Clayton <c_w_serious-github@yahoo.com.au>
2018-03-08 21:46:33 +1100

    Initial commit with calculation.js basic functions working correctly

---
+++ calculation.js
@@ -0,0 +1,21 @@
+const conversionFactor = 250;
+
+// Hard coded test data
+var itemLength_cm = 40;
+var itemWidth_cm  = 30;
+var itemHeight_cm = 20;
+
+// Convert item dimensions from centimetres to metres
+var itemLength_M = itemLength_cm / 100;
+var itemWidth_M  = itemWidth_cm  / 100;
+var itemHeight_M = itemHeight_cm / 100;
+
+console.log("itemLength_M:", itemLength_M);
+console.log("itemWidth_M:", itemWidth_M);
+console.log("itemHeight_M:", itemHeight_M);
+
+var itemCubicMetres = itemLength_M * itemWidth_M * itemHeight_M;
+var itemCubicWeight = itemCubicMetres * conversionFactor;
+
+console.log("item cubic metres: ", itemCubicMetres);
+console.log("cubic weight in Kg: ", itemCubicWeight);
