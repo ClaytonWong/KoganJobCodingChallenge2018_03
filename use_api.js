a6e52a82c9ed17f02751821de9b78e9c222a6a7c
Clayton <c_w_serious-github@yahoo.com.au>
2018-03-09 10:56:27 +1100

    Can get at an individual product, it's category and size

--- use_api.js
+++ use_api.js
@@ -2,12 +2,25 @@ const kogan = require('./api/kogan')
 
 kogan.getSomething('products/1')
   .then(res => {
-    const stuff = res.data
+    var stuff = res.data
+    console.log("--stuff---")
     console.log(stuff)
     //console.log(`Found ${stuff.length} things from kogan:`)
     //stuff.forEach(thing => {
     //  console.log(`${thing.name}`)
     //})
+    
+    var oneThing = stuff["objects"][0]
+    console.log("---oneThing---")
+    console.log(oneThing)
+
+    var oneThingCategory = stuff["objects"][0]["category"]
+    console.log("---oneThingCategory---")
+    console.log(oneThingCategory)
+
+    var oneThingCategorySize = stuff["objects"][0]["size"]
+    console.log("---oneThingCategorySize---")
+    console.log(oneThingCategorySize)
   })
   .catch((error) => {
     console.log('error from .catch in getSomething definition: ', error)
