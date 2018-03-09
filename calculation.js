ef2b2791c49482fe4a1cbad1119f3100b5e609e2
Clayton <c_w_serious-github@yahoo.com.au>
2018-03-09 15:35:31 +1100

    Add calCubicWeight function

--- calculation.js
+++ calculation.js
@@ -5,17 +5,29 @@ var itemLength_cm = 40;
 var itemWidth_cm  = 30;
 var itemHeight_cm = 20;
 
-// Convert item dimensions from centimetres to metres
-var itemLength_M = itemLength_cm / 100;
-var itemWidth_M  = itemWidth_cm  / 100;
-var itemHeight_M = itemHeight_cm / 100;
+function calCubicWeight( itemLength_cm, itemWidth_cm, itemHeight_cm) {
+  // Convert item dimensions from centimetres to metres
+  var itemLength_M = itemLength_cm / 100;
+  var itemWidth_M  = itemWidth_cm  / 100;
+  var itemHeight_M = itemHeight_cm / 100;
 
-console.log("itemLength_M:", itemLength_M);
-console.log("itemWidth_M:", itemWidth_M);
-console.log("itemHeight_M:", itemHeight_M);
+  /*
+  console.log("itemLength_M:", itemLength_M);
+  console.log("itemWidth_M:", itemWidth_M);
+  console.log("itemHeight_M:", itemHeight_M);
+  */
 
-var itemCubicMetres = itemLength_M * itemWidth_M * itemHeight_M;
-var itemCubicWeight = itemCubicMetres * conversionFactor;
+  var itemCubicMetres = itemLength_M * itemWidth_M * itemHeight_M;
+  var itemCubicWeight = itemCubicMetres * conversionFactor;
 
-console.log("item cubic metres: ", itemCubicMetres);
-console.log("cubic weight in Kg: ", itemCubicWeight);
+  /*
+  console.log("item cubic metres: ", itemCubicMetres);
+  console.log("cubic weight in Kg: ", itemCubicWeight);
+  */
+
+  return itemCubicWeight;
+}
+
+module.exports = { calCubicWeight }
+
+//console.log( `Test item cubic weight: ${calCubicWeight( itemLength_cm, itemWidth_cm, itemHeight_cm)} Kgs `)
--- use_api.js
+++ use_api.js
@@ -1,7 +1,45 @@
+import calCubicWeight from '/calculation'
+
 const kogan = require('./api/kogan')
 const startPoint = 'api/products/1'
 
+var currPage = startPoint // Initialise current page at starting point
+
 let airConRelArr = [] // Array for storing products in air conditioners category
+
+//while (currPage !== null) {
+    //var next = []
+/*   kogan.getPage(currPage)
+    .then(res => {
+      //while(currPage !== null) {
+        var page = res.data
+        var next = page["next"]
+        console.log("---currPage---")
+        console.log(currPage)
+        currPage = next
+        
+        kogan.getPage(currPage)
+        .then(res => {
+          //while(currPage !== null) {
+            var page = res.data
+            var next = page["next"]
+            console.log("---currPage---")
+            console.log(currPage)
+            currPage = next
+            
+          //}
+        })
+        .catch((error) => {
+          console.log('error from .catch in getSomething definition: ', error)
+        })
+
+      //}
+    })
+    .catch((error) => {
+      console.log('error from .catch in getSomething definition: ', error)
+    }) */
+  //currPage = next // Goto next page
+//}
 
 kogan.getPage(startPoint)
   .then(res => {
@@ -11,23 +49,20 @@ kogan.getPage(startPoint)
     for(i = 0; i < currPageProducts.length; i++) {
       // If current product is in air conditioners category,
       // add it to "air con related product array"
-      if (currPageProducts[i]["category"] == 'Air Conditioners') {
+      if (currPageProducts[i]["category"] === 'Air Conditioners') {
         airConRelArr.push(currPageProducts[i])
       }
     }
-
+    /*
     console.log("---airConRelArr---")
     console.log(airConRelArr)
+    */
+
+    airConRelArr.forEach((item) => {
+      console.log(item)
+    })
 
     /*
-    var oneThing = stuff["objects"][0]
-    console.log("---oneThing---")
-    console.log(oneThing)
-
-    var oneThingCategory = stuff["objects"][0]["category"]
-    console.log("---oneThingCategory---")
-    console.log(oneThingCategory)
-
     var oneThingCategorySize = stuff["objects"][0]["size"]
     console.log("---oneThingCategorySize---")
     console.log(oneThingCategorySize)
